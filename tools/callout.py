92445a41dbfc02a449da71328ba5e9ca5746f9f7
heussner <heussner@changlab-gpu1.ohsu.edu>
2023-04-13 14:03:58 -0700

    callout bug

--- tools/callout.py
+++ tools/callout.py
@@ -26,6 +26,10 @@ with open(args.table, 'rb') as handle:
 
 thresholds = args.PPR_thresholds
 markers = args.markers
+
+tCD45 = float(thresholds[markers.index('CD45')])
+tCK = float(thresholds[markers.index('CK')])
+
 print(markers)
 
 #remove cells without nuclei
@@ -43,43 +47,40 @@ if 's1' in args.table:
     print('Scene 1 skipped...')
 else: 
     #sort by CK PPR
-    sorted_table = table.sort_values(by=['cell_'+'CK'+'_PPR'],ascending=False,ignore_index=True).copy()
+    sorted_table = table.sort_values(by=['cell_CK_PPR'],ascending=False,ignore_index=True).copy()
+    
     #remove cells below CK threshold
-    CK_positives = sorted_table[sorted_table['cell_'+'CK'+'_PPR'] >= float(thresholds[markers.index('CK')])].copy()
+    CK_positives = sorted_table[sorted_table['cell_CK_PPR'] >= tCK].copy()
 
     #remove cells below CD45 threshold
-    CHCs = CK_positives[CK_positives['cell_CD45_PPR'] >= float(thresholds[markers.index('CD45')])].copy()
+    CHCs = CK_positives[CK_positives['cell_CD45_PPR'] >= tCD45].copy()
 
     #record number of CHCs, index of last CHC, get list of CHC cell labels
     n = len(CHCs)
     print(f'Found {n} CHCs')
-    idx = CHCs.axes[0].tolist()[-1]
+    idx = CK_positives.axes[0].tolist()[-1]
     CHC_labels = CHCs['cell_label'].tolist()
 
     #remove CHCs from original sorted table
     CHCs_removed = sorted_table.loc[idx+1:len(sorted_table)].copy()
 
     #filter out CD45 negatives
-    CD45_positives = CHCs_removed[CHCs_removed['cell_CD45_PPR'] >= float(thresholds[markers.index('CD45')])].copy()
+    CD45_positives = CHCs_removed[CHCs_removed['cell_CD45_PPR'] >= tCD45].copy()
 
     #get sorted remaining cell labels
-    other_labels = CD45_positives['cell_label'].tolist()
-
-    #get near non-CHC cell labels and random cell labels 
-    non_CHC_labels = other_labels[0:n]
+    near_CHC_labels = CD45_positives['cell_label'].tolist()[0:n]
     
     #get negative controls, sorted by CD45 mean intensity
-    Negative_controls = table[table['cell_'+'CK'+'_PPR_raw'] == 0].copy()
-    Negative_controls = Negative_controls[Negative_controls['cell_'+'CD45'+'_PPR'] < 0.7]
+    Negative_controls = table[table['cell_CK_PPR_raw'] == 0].copy()
+    Negative_controls = Negative_controls[Negative_controls['cell_CD45_PPR'] < tCD45]
     Negative_controls = Negative_controls.sort_values(by=['cell_CD45_mean_intensity'], ascending=True)
-    Negative_control_labels = Negative_controls['cell_label'].tolist()[0:n]
-    print(f'Found {n} negative cells')
+    negative_labels = Negative_controls['cell_label'].tolist()[0:n]
     
     #(P)-positive CHCs 
     #(G)-grey area CHCs
     #(N)-grey area CHCs
     #save; order matters in these lists!
-    label_dict = {"P":CHC_labels,"G":non_CHC_labels,"N":Negative_control_labels}
+    label_dict = {"P":CHC_labels,"G":near_CHC_labels,"N":negative_labels}
 
 with open(os.path.join(args.output,args.save_prefix+'_CALLOUTS.pkl'), 'wb') as handle:
     pickle.dump(label_dict, handle)
