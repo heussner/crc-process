26911eee49365c5ead71f9a4c5ca6c4d1cf9a4d2
heussner <heussner@changlab-gpu1.ohsu.edu>
2024-04-17 11:46:43 -0700

    remove slurm as nextflow executor

--- setup.sh
+++ setup.sh
@@ -10,7 +10,6 @@ export PATH="/opt/singularity/current/bin:$PATH"
 export SINGULARITY_CACHEDIR=${NXF_HOME}
 export NXF_SINGULARITY_CACHEDIR=${NXF_HOME}
 export NXF_OPTS="-Xms500M -Xmx64G"
-export NXF_EXECUTOR=slurm
 GIT_HASH="6266d7a3624407997846505f883d9be4612ec496"
 
 nextflow pull labsyspharm/mcmicro
